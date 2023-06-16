b405513cf78b90ed36e123c08daa8258b6e60686
Samuk <samukcarlos@gmail.com>
2023-06-16 19:31:46 -0300

    Cat request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,21 +2,37 @@ import './styles.css';
 import ButtonPrimary from "../../../components/ButtonInverse";
 import ButtonInverse from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from "../../../services/product-service"; 
-import { useParams } from 'react-router-dom';
+import * as productService from '../../../services/product-service';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
 
 export default function ProductDetails(){
 
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
-  
+
+  const navigate = useNavigate();
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    productService.findById(Number(params.productId))
+      .then(response => {
+          setProduct(response.data);
+      })
+      .catch(() => {
+          navigate("/");
+      });
+   }, []);   
   return (            
     <main>
       <section id="product-details-section" className="dsc-container">
         {
             product &&
             <ProductDetailsCard product={product} />
+           // ? <ProductDetailsCard product={product} />
+           // : <h2>Código inválido</h2>
         }
         
         <div className="dsc-btn-page-container">
