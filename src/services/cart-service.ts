34824b55fb1520625bac8c391766ef7d019046e8
Samuk <samukcarlos@gmail.com>
2023-06-20 21:20:56 -0300

    Cart save/get

---
+++ src/services/cart-service.ts
@@ -0,0 +1,10 @@
+import { OrderDTO } from "../models/order";
+import * as cartRepository from '../localstorage/cart-repository';
+
+export function saveCart(cart: OrderDTO){
+         cartRepository.save(cart);
+}
+
+export function getCar() : OrderDTO{
+    return cartRepository.get();
+}
