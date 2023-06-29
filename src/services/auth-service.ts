bd19133d1bcd0728f5bc229dca6772fbe5e883e5
Samuk <samukcarlos@gmail.com>
2023-06-29 20:16:30 -0300

    hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,5 +1,5 @@
 import QueryString from "qs";
-import { CredentialsDTO, accessTokenPayloadDTO } from "../models/auth";
+import { CredentialsDTO, RoleEnum, accessTokenPayloadDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
@@ -47,9 +47,24 @@ export function loginRequest(loginData: CredentialsDTO){
         }
     export function isAuthenticated(): boolean {
         let tokenPayload = getAccessTokenPayload();
-        if (tokenPayload && tokenPayload.exp * 1000 > Date.now()){
+            if (tokenPayload && tokenPayload.exp * 1000 > Date.now()){
             return true;
         }
 
         return   false;
     }
+    export function hasAnyRoles(roles: RoleEnum[]): boolean {
+        if (roles.length === 0) {
+        return true;
+        }
+        const tokenPayload = getAccessTokenPayload();
+        if (tokenPayload !== undefined) {
+            for (var i = 0; i < roles.length; i++) {
+                if (tokenPayload.authorities.includes(roles[i])) {
+                    return true;
+        }
+        }
+        //return roles.some(role => tokenData.authorities.includes(role));
+        }
+        return false;
+        }
