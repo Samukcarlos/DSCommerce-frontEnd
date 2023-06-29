01c9a00ba4e4a4a04be7f2f7eea5c3107c8ab7ac
Samuk <samukcarlos@gmail.com>
2023-06-28 21:10:54 -0300

    requestNackend update withCredentials

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,11 +1,11 @@
+import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
-import * as authService from './auth-service';
 
 export function findMe() {
 
-    const headers = {
-        Authorization: "Bearer " + authService.getAccessToken()
+    const config : AxiosRequestConfig = {
+        url: "/users/me",
+        withCredentials: true
     }
-        console.log(headers);
-    return requestBackend({url: `/users/me`, headers});
+    return requestBackend(config);
 }
