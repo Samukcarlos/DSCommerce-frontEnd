dae9153379d5eedfa926e8bb8d97abf8c1e2b673
Samuk <samukcarlos@gmail.com>
2023-06-30 20:11:21 -0300

    Dynamic product listing

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -1,9 +1,35 @@
 import './styles.css'
+import * as productService from '../../../services/product-service'
 import editIcon from '../../../assets/edite.svg'
 import deleteIcon from '../../../assets/delite.svg'
-import product from '../../../assets/computer.png'
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product'
 
+type QueryParams ={
+  page: number;
+  name: string;
+}
 export default function ProductListing(){
+
+  const [isLestPage, setIsLestPage]= useState(false);  // é a ultama página? caso seja botão carregar mais some
+
+  const [products, setProduct]= useState<ProductDTO[]>([]);
+
+  const [queryParams, setQueryParams]= useState<QueryParams>({
+    page: 0,
+    name: ""
+  });
+
+  useEffect(()=> {
+    productService.findPageRequest(queryParams.page, queryParams.name)
+    .then(response => {
+        const nextPage = response.data.content;
+        setProduct(products.concat(nextPage)); //acrescentando itens na página com o carregar mais
+        setIsLestPage(response.data.last);
+      });
+
+}, [queryParams]); // mudando o status/valor a função é executada (productService)
+
     return(
         <main>
         <section id="product-listing-section" className="dsc-container">
@@ -31,33 +57,19 @@ export default function ProductListing(){
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="dsc-tb576">341</td>
-                <td><img className="dsc-product-listing-image" src={product} alt="Computer"/></td>
-                <td className="dsc-tb768">R$ 5000,00</td>
-                <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-                <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
-              </tr>
-              <tr>
-                <td className="dsc-tb576">341</td>
-                <td><img className="dsc-product-listing-image" src={product} alt="Computer"/></td>
 
-                <td className="dsc-tb768">R$ 5000,00</td>
-                <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-                <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
-              </tr>
-              <tr>
-                <td className="dsc-tb576">341</td>
-                <td><img className="dsc-product-listing-image" src={product} alt="Computer"/></td>
-
-                <td className="dsc-tb768">R$ 5000,00</td>
-                <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
-                <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
-                <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
-              </tr>
-             
+                {
+                  products.map(product => (
+                    <tr>
+                    <td className="dsc-tb576">{product.id}</td>
+                    <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name}/></td>
+                    <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
+                    <td className="dsc-txt-left">{product.name}</td>
+                    <td><img className="dsc-product-listing-btn" src={editIcon} alt="Editar"/></td>
+                    <td><img className="dsc-product-listing-btn" src={deleteIcon} alt="Deletar"/></td>
+                  </tr>
+                    ))
+                  }             
             </tbody>
           </table>
   
