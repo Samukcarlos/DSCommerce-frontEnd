3f18be4a12359ce500e9b68e8b3bd653b1b33196
Samuk <samukcarlos@gmail.com>
2023-07-01 17:14:29 -0300

    Custon FormInput

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -5,6 +5,7 @@ import { CredentialsDTO } from '../../../models/auth';
 import * as authService from '../../../services/auth-service';
 import { useNavigate } from 'react-router-dom';
 import { ContextToken } from '../../../utils/context-token';
+import FormInput from '../../../components/FormInput';
 
 export default function Login(){
 
@@ -12,14 +13,30 @@ export default function Login(){
 
   const navigate = useNavigate();
   
-  const [formData, setFormData]= useState<CredentialsDTO>({
-    username: '',
-    password: ''
-  })
+  const [formData, setFormData] = useState<any>({
+    username: {
+    value: "",
+    id: "username",
+    name: "username",
+    type: "text",
+    placeholder: "Email",
+    validation: function (value: string) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value.toLowerCase());
+    },
+    message: "Favor informar um email válido",
+    },
+    password: {
+    value: "",
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "Senha",
+    }
+    })
 
   function hendleSubmit(event: any){
     event.preventDefault();
-    authService.loginRequest(formData)
+    authService.loginRequest({username: formData.username.value, password: formData.password.value})
       .then(response => {
         authService.saveAccessToken(response.data.access_token);
         setContextTokenPayload(authService.getAccessTokenPayload());
@@ -33,7 +50,7 @@ export default function Login(){
   function handleInputChange(event:any){
     const value = event.target.value;
     const name = event.target.name;
-    setFormData ({...formData, [name]:value});
+    setFormData ({...formData, [name]: {...formData[name], value:value}});
   }
 
     return(
@@ -44,23 +61,17 @@ export default function Login(){
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input 
+                <FormInput
+                {...formData.username}
                 className="dsc-form-control" 
-                name= "username"
-                value={formData.username}
-                type="text" 
-                placeholder="Email"
                 onChange={handleInputChange}
                 />                
                 <div className="dsc-form-error"></div>
               </div>
               <div>
-                <input 
+                <FormInput 
+                {...formData.password}
                 className="dsc-form-control" 
-                name='password'
-                value={formData.password}
-                type="password" 
-                placeholder="Senha"
                 onChange={handleInputChange}
                 />
               </div>
