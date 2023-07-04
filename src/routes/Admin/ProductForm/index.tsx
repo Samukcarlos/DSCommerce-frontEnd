50ad67be75017d8fa6307151dc73bdb6a1a8e233
Samuk <samukcarlos@gmail.com>
2023-07-04 20:46:00 -0300

    forms validate

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -26,6 +26,10 @@ export default function ProductForm(){
             name: "price",
             type: "number",
             placeholder: "Preço",
+            validation: function(value: any){
+                return Number(value)>0;
+            },
+            massage: "Favor informar um valor positivo"
           },
     imgUrl: {
             value: "",
